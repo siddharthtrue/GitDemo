a7e6d1c626a5bc227d44373ada8d9a5783487bcc
siddharth <shahsid84@gmail.com>
2022-02-14 18:15:54 +0530

    first commit

---
+++ src/test/java/RSA/RSA_Maven/AppiumTest.java
@@ -0,0 +1,18 @@
+package RSA.RSA_Maven;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest
+{
+	@Test
+	public void c_startAppium()
+	{
+		System.out.println("start Appium");
+	}
+	
+	@Test
+	public void d_endAppium()
+	{
+		System.out.println("end Appium");
+	}
+}
---
+++ src/test/java/RSA/RSA_Maven/SeleniumTest.java
@@ -0,0 +1,18 @@
+package RSA.RSA_Maven;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest 
+{
+	@Test
+	public void a_startBrowser()
+	{
+		System.out.println("start browser");
+	}
+	
+	@Test
+	public void b_endBrowser()
+	{
+		System.out.println("end browser");
+	}
+}
